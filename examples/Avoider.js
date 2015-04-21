80a6363e1c98fb116eb65011758f6ae84639786e
Raymond Cook <raymond@weeby.co>
2015-04-21 12:49:51 -0700

    Updating avoider demo to work with scene

--- examples/Avoider.js
+++ examples/Avoider.js
@@ -1,43 +1,48 @@
-import scene, art;
+import scene, effects, communityart;
 
 /**
-  * @requires scene 0.0.1
+  * @requires scene 0.1.9
   */
 scene.splash(function() {
   scene.addText('Avoider: Tap to start!');
-  scene.addBackground(art('starscape_2');
+  scene.addBackground(communityart('starscape'));
 });
 
-scene(function() {
-  var bg = scene.addBackgroundLayer(art('starcape_1'), { scroll: 0.01 });
-  var player = scene.addPlayer(art('rocketship'), {
-    followTouches: true,
-    zIndex: 10
+exports = scene(function() {
+  var bg = scene.addBackground(communityart('starscape'));
+
+  var player = scene.addPlayer(communityart('rocket_ship'), {
+    followTouches: { x: true, y: true }
   });
+
   scene.showScore(10, 10);
 
+  var enemies = scene.addGroup();
+
   // Set up enemies
-  var enemySpawner = scene.addSpawner(new scene.spawner.Spawner(
+  var enemySpawner = scene.addSpawner(new scene.spawner.Timed(
     [
-      new scene.shape.Line({ x: 0, y: scene.screen.height, y2: 0 }),
-      new scene.shape.Line({ x: 0, y: 0, x2: scene.screen.width, }),
+      new scene.shape.Line({ x: -100, y: scene.screen.height, y2: 0 }),
+      new scene.shape.Line({ x: 0, y: -100, x2: scene.screen.width }),
       new scene.shape.Line({ x: scene.screen.width, y: 0, y2: scene.screen.height })
     ],
     function (x, y, index) {
-      var enemy = scene.addActor(art('angry_bullet'));
-      enemy.onEscaped(scene.screen, enemy.destroy);
-
-      var randomPoint = scene.screen.getPointOn();
-      enemy.headToward(randomPoint.x, randomPoint.y, randRange(10, 20));
-
-      return enemy;
-    },
-    { spawnDelay: 750 }
+      var enemy = enemies.addActor(communityart('enemy_ship'), { x: x, y: y });
+      enemy.onExited(scene.camera, function() { enemy.destroy(); });
+      enemy.headToward(player.x, player.y, randRange(100, 200));
+    }, 750
   ));
-  scene.onCollision(player, enemySpawner, player.destroy);
+
+  scene.onCollision(player, enemies, function() {
+    effects.shake(GC.app);
+    effects.explode(player);
+    player.destroy();
+  });
+
+  var startTime = scene.totalDt;
 
   // Add points with time
   scene.onTick(function(dt) {
-    scene.addScore(Math.round(dt / 10));
+    scene.setScore(Math.floor((scene.totalDt - startTime) * 0.001));
   });
 });
