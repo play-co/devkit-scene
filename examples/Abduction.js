95e9acb9c7523366f036320858c67946429248d9
Jimmy Griffith <rogueSkib@gmail.com>
2015-05-18 18:17:53 -0700

    updated Abduction example

--- examples/Abduction.js
+++ examples/Abduction.js
@@ -3,13 +3,12 @@
   * @see https://play.google.com/store/apps/details?id=au.com.phil&hl=en
   * @requires scene 0.1.9
   */
-import scene, communityart;
+import scene;
+import communityart as art;
 
-exports = scene(function() {
-  // Add the background and the player
-  var background = scene.addBackground(communityart('abduction/bg'));
-
-  var player = scene.addPlayer(communityart('abduction/player'), {
+exports = scene(function () {
+  var background = scene.addBackground(art('abduction/bg'));
+  var player = scene.addPlayer(art('abduction/player'), {
     ay: 2000,
     vy: -2400,
     followTouches: { x: true },
@@ -17,22 +16,27 @@ exports = scene(function() {
     cameraFunction: scene.camera.wrapX
   });
 
-  scene.onAccelerometer(function(e) { player.vx = -e.tilt * 3000; });
-
-  // Show the game score
   scene.showScore(10, 10);
+  scene.onAccelerometer(function (e) {
+    player.vx = -e.tilt * 3000;
+  });
 
   var platforms = scene.addGroup();
-
-  var platformSpawnFunction = function(x, y, index) {
-    var platform = platforms.addActor(communityart('abduction/platform'), { isAnchored: true, x: x, y: y });
-    platform.onEntered(scene.camera.bottomWall, function() { platform.destroy(); });
+  var platformSpawnFunction = function (x, y, index) {
+    var platform = platforms.addActor(art('abduction/platform'), { x: x, y: y, fixed: true });
+    platform.onEntered(scene.camera.bottomWall, function () {
+      platform.destroy();
+    });
   };
 
-  // Make the spawner
   var platformSpawner = scene.addSpawner(
     new scene.spawner.Vertical(
-      new scene.shape.Rect({ x: 75, y: -300, width: scene.screen.width - 75, height: 200}),
+      new scene.shape.Rect({
+        x: 75,
+        y: -300,
+        width: scene.screen.width - 75,
+        height: 200
+      }),
       platformSpawnFunction,
       200
     )
@@ -42,24 +46,28 @@ exports = scene(function() {
   scene.onCollision(player, platforms, function (player, platform) {
     if (player.vy < 0) { return; }
     // If last frame's player collision bottom was above the platform, hop
-    var lastCollisionBottom = player.shape.bounds.maxY - (player.y - player.previousY);
-    if (lastCollisionBottom < platform.shape.bounds.minY) {
+    var lastCollisionBottom = player.shape.maxY - (player.y - player.previousY);
+    if (lastCollisionBottom < platform.shape.minY) {
       player.vy = -1350;
     }
   }, true);
 
-  player.onEntered(scene.camera.bottomWall, function() { player.destroy(); });
+  player.onEntered(scene.camera.bottomWall, function () {
+    player.destroy();
+  });
 
   // Add the camera to follow the player
   scene.camera.follow(player,
     new scene.shape.Rect({
-      x: -400, y: scene.screen.center.y,
-      width: scene.screen.width + 800, height: scene.screen.height
+      x: -400,
+      y: scene.screen.centerY,
+      width: scene.screen.width + 800,
+      height: scene.screen.height
     })
   );
 
   // Update the score
-  scene.onTick(function() {
+  scene.onTick(function () {
     scene.setScore(Math.floor(-scene.camera.y));
   });
 });
