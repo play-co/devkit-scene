a5f8c2a9fd71ec4eabc56d57ac4b7ab105bd1666
yofreke <sparkyjoe94@aim.com>
2015-06-29 17:31:41 -0700

    added new subject based animation supporot from #18

--- src/animation/index.js
+++ src/animation/index.js
@@ -1,6 +1,32 @@
 import animate;
 import animate.transitions as transitions;
 
+/**
+ * Map of subject instances -> animation groupId
+ * @type {Object}
+ */
+var subjectAnimations = {}
+
+var addSubjectAnimation = function(subject, groupId) {
+  if (!subjectAnimations[subject]) {
+    subjectAnimations[subject] = [groupId];
+  } else {
+    var animationGroups = subjectAnimations[subject];
+    if (animationGroups.indexOf(groupId) === -1) {
+      animationGroups.push(groupId);
+    }
+  }
+};
+
+this.clearSceneAnimations = function(subject) {
+  var animationGroups = subjectAnimations[subject];
+  for (var i = 0, len = animationGroups.length; i < len; i++) {
+    animate(subject, animationGroups[i]).clear();
+  }
+
+  delete subjectAnimations[subject];
+};
+
 exports = {
   /**
    * Easy access to {@link animate.transitions}
@@ -16,26 +42,32 @@ exports = {
   animationGroups: null,
 
   /**
-   * Clear all the tracked animation groups
+   * Clear all the tracked animation groups, or a specific subject's animations
    * @method scene.clearAnimations
+   * @param {Object} [subject] - optionally clear animations for a specific subject
    */
-  clearAnimations: function() {
-    if (scene.animationGroups) {
-      // Clear old animaion groups
-      for (var i = 0; i < scene.animationGroups.length; i++) {
-        var group = animate.getGroup(scene.animationGroups[i]);
-        if (!group) { continue; }
-        var animations = group._anims;
-        for (var key in animations) {
-          animations[key].commit();
-          animations[key].clear();
-          delete animations[key];
+  clearAnimations: function(subject) {
+    if (subject) {
+      clearSubjectAnimations(subject);
+    } else {
+      if (this.animationGroups) {
+        // Clear old animaion groups
+        for (var i = 0; i < this.animationGroups.length; i++) {
+          var group = animate.getGroup(this.animationGroups[i]);
+          if (!group) { continue; }
+          var animations = group._anims;
+          for (var key in animations) {
+            animations[key].commit();
+            animations[key].clear();
+            delete animations[key];
+          }
         }
       }
-    }
 
-    // Reset array
-    scene.animationGroups = ['scene'];
+      // Reset
+      this.animationGroups = ['scene'];
+      subjectAnimations = {};
+    }
   },
 
   /**
@@ -47,9 +79,13 @@ exports = {
   animate: function(subject, groupId) {
     groupId = groupId === undefined ? "scene" : "scene_" + groupId;
     var anim = animate(subject, groupId);
-    if (groupId !== "scene" && scene.animationGroups.indexOf(groupId) === -1) {
-      scene.animationGroups.push(groupId);
+    if (groupId !== "scene" && this.animationGroups.indexOf(groupId) === -1) {
+      this.animationGroups.push(groupId);
     }
+
+    // For individual subject animation tracking
+    addSubjectAnimation(subject, groupId);
+
     return anim;
   },
 
