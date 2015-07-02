f5d80149ecb03794894ff753cc5db400a8a4a822
Jimmy Griffith <rogueSkib@gmail.com>
2015-07-02 16:11:25 -0700

    ui views allow data from community art 	instead of only via fn parameters

--- src/ui/UIView.js
+++ src/ui/UIView.js
@@ -6,18 +6,28 @@ import .ProgressBar;
 exports = Class(BaseView, function(supr) {
 
   this.addProgressBar = function(resource, x, y) {
-    var opts = merge({ superview: this, x: x, y: y }, resource);
-    return new ProgressBar(opts);
+    var opts = { superview: this };
+    x !== undefined && (opts.x = x);
+    y !== undefined && (opts.y = y);
+    return new ProgressBar(merge(opts, resource));
   };
 
   this.addScoreText = function(resource, x, y, width, height) {
-    var opts = merge({ superview: this, x: x, y: y, width: width, height: height, format: SceneScoreView.FORMAT_SCORE }, resource);
-    return new SceneScoreView(opts);
+    var opts = { superview: this, format: SceneScoreView.FORMAT_SCORE };
+    x !== undefined && (opts.x = x);
+    y !== undefined && (opts.y = y);
+    width !== undefined && (opts.width = width);
+    height !== undefined && (opts.height = height);
+    return new SceneScoreView(merge(opts, resource));
   };
 
   this.addTimeText = function(resource, x, y, width, height) {
-    var opts = merge({ superview: this, x: x, y: y, width: width, height: height, format: SceneScoreView.FORMAT_TIME }, resource);
-    return new SceneScoreView(opts);
+    var opts = { superview: this, format: SceneScoreView.FORMAT_TIME }
+    x !== undefined && (opts.x = x);
+    y !== undefined && (opts.y = y);
+    width !== undefined && (opts.width = width);
+    height !== undefined && (opts.height = height);
+    return new SceneScoreView(merge(opts, resource));
   };
 
   this.reset = function() {
