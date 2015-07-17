64ab2e064a779638f62ac35476a1bee3c4333d91
Jimmy Griffith <rogueSkib@gmail.com>
2015-07-16 20:27:39 -0700

    first pass on StateManager enhancements

--- src/state/StateManager.js
+++ src/state/StateManager.js
@@ -18,18 +18,40 @@ exports = Class(function() {
     /** @type boolean
         @private */
     this._enteringState = false;
+    /** @type number
+        @private */
+    this._order = 0;
 
     /** @type object */
     this.gameData = null;
   };
 
   /**
-   * Add a new state by name
-   * @param {string}       stateName
-   * @param {stateSetupFn} setupFn
+   * Add new state by name; must provide onEnter function to build the state
+   * @param {string}   name
+   * @param {function} onEnter
+   * @param {object}   [opts]
+   * @param {number}   [opts.order] By default, states appear in the order added; this option allows you to specify a different order
+   * @param {boolean}  [opts.tapToContinue] Adds a touch handler to automatically proceed to the next state
+   * @param {string}   [opts.nextState] Supercedes order to force a specifc state on transition
    */
-  this.add = function(name, initializer) {
-    this._states[name] = initializer;
+  this.add = function(name, onEnter, opts) {
+    opts = opts || {};
+    this._states[name] = {
+      name: name,
+      onEnter: onEnter,
+      order: opts.order !== undefined ? opts.order : this._order++,
+      tapToContinue: opts.tapToContinue || false,
+      nextState: opts.nextState || ''
+    };
+  };
+
+  /**
+   * Remove an existing state by name
+   * @param {string} name
+   */
+  this.remove = function(name) {
+    delete this._states[name];
   };
 
   /**
@@ -39,7 +61,7 @@ exports = Class(function() {
    */
   this.has = function(name) {
     return !!this._states[name];
-  }
+  };
 
   /**
    * Reset the game data and enter the default state
@@ -102,6 +124,75 @@ exports = Class(function() {
    */
   this.onExit = function(exitFunction) {
     this._exitFunctions.push(exitFunction);
+  };
+
+  /**
+   * Shortcut for state transition on user input
+   * @param {string} nextState
+   */
+  this.tapToContinue = function(nextState) {
+    scene.screen.onUp(function() {
+      if (nextState) {
+        this.enter(nextState);
+      } else {
+        this.next();
+      }
+    }, true);
+  };
+
+  /**
+   * Transition to the next state
+   */
+  this.next = function() {
+    this.enter(this._getNextStateName());
+  };
+
+  /**
+   * Return the name of the state to transition to next
+   * @private
+   */
+  this._getNextStateName = function() {
+    var index = 0;
+    var next = '';
+    var first = '';
+    var currData = this._states[this.currentState];
+
+    // use order by default, superceded by nextState
+    if (currData) {
+      if (currData.nextState) {
+        next = currData.nextState;
+      } else {
+        index = currData.order + 1;
+      }
+    }
+
+    // find the next state based on the order property
+    var nextIndex = index;
+    var firstIndex = Infinity;
+    if (next === '') {
+      for (var name in this._states) {
+        var data = this._states[name];
+        if (data) {
+          // find the next state in order
+          if (data.order >= index && data.order <= nextIndex) {
+            next = name;
+            nextIndex = data.order;
+          }
+          // find the first state in case next isn't found
+          if (data.order <= firstIndex) {
+            first = name;
+            firstIndex = data.order;
+          }
+        }
+      }
+    }
+
+    // if we're out of states, loop around to the first
+    if (next === '') {
+      next = first;
+    }
+
+    return next;
   };
 
   /**
