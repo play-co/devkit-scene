12184c402516944110b8797709d446a7f778abdd
yofreke <sparkyjoe94@aim.com>
2015-08-04 00:00:27 -0700

    added an example for custom images

---
+++ examples/CustomImage.js
@@ -0,0 +1,22 @@
+import scene, effects;
+import communityart as art;
+
+// Community art is a helper class for separating game code from art definitions.
+// You must register configs using the registerConfig function. The first argument
+// is the key for this art asset, and the second argument is the config object.
+art.registerConfig('myArt', {
+  type: 'ImageView',
+  opts: {
+    url: 'http://i.imgur.com/aok0WIJb.jpg'
+  }
+});
+
+exports = scene(function() {
+
+  // Many scene functions expect a config object as the first parameter.
+  // Caling communityart as a function, passing the desired key, will return the art
+  // config that was registered earlier.
+  var myArtConfig = art('myArt');
+  var image = scene.addImage(myArtConfig, 150, 150);
+
+});
