697af244e5b499bd71196fe47318962b5a2bcb78
Jimmy Griffith <rogueSkib@gmail.com>
2015-08-24 21:14:21 -0700

    remove SAT support, devkit-entities should solve ActorTemplate -> Actor

--- src/actor/SATActor.js
+++
@@ -1,9 +0,0 @@
-import .ActorTemplate;
-import entities.SAT.Entity as SATEntity;
-
-/**
- * For all documentation see {@link ActorTemplate}
- * @class SATActor
- * @extends {SATEntity}
- */
-exports = ActorTemplate(SATEntity);
